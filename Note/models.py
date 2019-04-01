0f4a26998972a29a96f284dd935ef2ed735bddb2
xiejingbao <xiejb@qlzqzg.com>
2019-04-01 19:07:10 +0800

    complete

--- Note/models.py
+++ Note/models.py
@@ -1,14 +1,20 @@
+
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
 class UserInfo(AbstractUser):
     id = models.CharField(primary_key=True,unique=True,max_length=20)
     status = models.CharField('状态1未审核2审核中3审核通过',max_length=1,default=1)
     phone = models.CharField(max_length=11)
-    createTime = models.DateTimeField('创建日期',auto_now_add=True)
+
+    createTime = models.DateTimeField('创建日期',default=timezone.now)
     updateTime = models.DateTimeField('修改日期',auto_now=True)
-    end_vip_time = models.DateTimeField('vip到期日')
+    end_vip_time = models.DateTimeField('vip到期日',auto_now=True)
     business_flag = models.CharField('1营业中，2暂停营业',max_length=1)
     image_url = models.CharField(max_length=100)
     image_url_id = models.CharField(max_length=20)
@@ -21,9 +27,9 @@ class MerchantInfo(models.Model):
     phone = models.CharField(max_length=11)
     name  = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
-    createTime = models.DateTimeField('创建日期', auto_now_add=True)
+    createTime = models.DateTimeField('创建日期', default=timezone.now)
     updateTime = models.DateTimeField('修改日期', auto_now=True)
-    end_vip_time = models.DateTimeField('vip到期日')
+    end_vip_time = models.DateTimeField('vip到期日',auto_now=True)
     business_flag = models.CharField('1营业中，2暂停营业', max_length=1)
     image_url = models.CharField(max_length=100)
     desc = models.CharField(max_length=100)
@@ -37,7 +43,7 @@ class Food(models.Model):
     desc = models.CharField(max_length=100)
     type = models.CharField(max_length=1)
     status = models.CharField('上架，2下架',max_length=1)
-    createTime = models.DateTimeField('创建日期', auto_now_add=True)
+    createTime = models.DateTimeField('创建日期', default=timezone.now)
     updateTime = models.DateTimeField('修改日期', auto_now=True)
 
 class Images(models.Model):
@@ -46,5 +52,5 @@ class Images(models.Model):
     image_url = models.CharField(max_length=100)
     food_id = models.CharField(max_length=20)
     user_id = models.CharField(max_length=20)
-    createTime = models.DateTimeField('创建日期', auto_now_add=True)
+    createTime = models.DateTimeField('创建日期',default=timezone.now)
     updateTime = models.DateTimeField('修改日期', auto_now=True)
--- Note/views.py
+++ Note/views.py
@@ -1,17 +1,74 @@
+import json
+
+from django.contrib import auth
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+import logging
 from Note import models
+from Note.forms import VmaigUserCreationForm
 
+logger = logging.getLogger(__name__)
 #个人注册
+
 def  registPersonal(request):
-    models.UserInfo.username
+
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    form = VmaigUserCreationForm(request.POST)
+    errors = []
+
+    if request.method == 'POST':
+      if form.is_valid():
+         form.save()
+         return res(request, "true")
+      else:
+          for k, v in form.errors.items():
+              # v.as_text() 详见django.forms.util.ErrorList 中
+              errors.append(v.as_text())
+      mydict = {"errors": errors}
+      return res(request,"false",mydict)
+    return render(request, 'personal_user/regist.html', {'obj': form})
+
+
 
 def  loginPersonal(request):
-    models.UserInfo.username
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+    user = auth.authenticate(username=username, password=password)
+
+    errors = []
+
+    if user is not None:
+        auth.login(request, user)
+    else:
+        errors.append(u"密码或者用户名不正确")
+
+    mydict = {"errors": errors}
+    return HttpResponse(
+        json.dumps(mydict),
+        content_type="application/json"
+    )
+
+def logout(self, request):
+    if not request.user.is_authenticated():
+        logger.error(u'[UserControl]用户未登陆')
+        raise PermissionDenied
+    else:
+        auth.logout(request)
+        return HttpResponse('OK')
 
 def  updatePersonal(request):
     models.UserInfo.username
 
 def  list(request):
     models.UserInfo.username
+
+
+def res(request,success,data,code,msg):
+    result = {"success":success,"data":data,"errorCode":code,"msg":msg}
+    #json返回为中文
+    return HttpResponse(json.dumps(result,ensure_ascii=False),content_type="application/json,charset=utf-8")
